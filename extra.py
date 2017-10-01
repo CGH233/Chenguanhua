a88b8a508bcf53350399fd1008436b2762010b0e
CGH233 <1113713599@qq.com>
2017-10-01 09:11:41 +0800

    Chenguahua

--- extra.py
+++ extra.py
@@ -1,8 +1,18 @@
-n=input("输入一个正整数：")
+# -*- coding: utf-8 -*-
+
+n=input("输入一个小于1000的正整数：")
 n=int(n)
+a=0
+a=int(a)
 if n>1000:
     print("Error")
 else:
     while n>1:
-        
+        if (n%2)==0:
+             n=n/2
+             a=a+1
+        else:
+             n=(3*n+1)/2
+             a=a+1
+    print(a)               
     
