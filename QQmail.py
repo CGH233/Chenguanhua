ac1015f8b404961de788769b399cee442c90fc36
CGH233 <1113713599@qq.com>
2017-10-31 22:58:14 +0800

    qqmail

---
+++ QQmail.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+
+app.config.update(
+    #EMAIL SETTINGS
+    MAIL_SERVER='smtp.qq.com',
+    MAIL_PORT=465,
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME = '1113713599',
+    MAIL_PASSWORD = 'cgh12362202687,.'
+    )
+
+mail = Mail(app)
+
+@app.route("/")
+def index():
+    msg = Message(subject="Hello",
+                  sender='you@qq.com',
+                  recipients=['recipient@recipient_domain.com'])
+    msg.html = "<b>testing</b> html"
+
+    mail.send(msg)
+
+    return '<h1>Sent</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
