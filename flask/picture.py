256fd88e054e1448d576d00864c151c47dbe99de
CGH233 <1113713599@qq.com>
2019-05-31 23:49:01 +0800

    feat:little demo

--- flask/picture.py
+++ flask/picture.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-local_list = ['东北', '华北', '华东', '华南', '西北', '西南']
-html_list = ['2019年东北地区AQI影响指标雷达图.html', '2019年华北地区AQI影响指标雷达图.html', '2019年华东地区AQI影响指标雷达图.html', '2019年华南地区AQI影响指标雷达图.html', '2019年西北地区AQI影响指标雷达图.html', '2019年西南地区AQI影响指标雷达图.html']
+local_list = ['dongbei', 'huabei', 'huadong', 'huanan', 'xibei', 'xinan']
+html_list = ['1.html', '2.html', '3.html', '4.html', '5.html', '6.html']
 
 @app.route('/<local>')
 def picture(local):
